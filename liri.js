36fe1e6ca10be807319a9d3785f0e156b4d5f079
Charles Tuggle <tuggle.charles@gmail.com>
2019-10-15 19:07:57 -0400

    commit with lots of updates to functionality. still need to fix: concert date display, command string that can take in more than one word, and movie functionaity

--- liri.js
+++ liri.js
@@ -1,46 +1,120 @@
+// Variable for requiring dotenv package //
 require("dotenv").config();
 
+// Variable for requiring keys file //
 var keys = require("./keys.js");
 
-// variables for requiring node packages for each API //
+// Variable for requiring node package to read and write files //
+var fs = require("fs");
+
+// Variables for requiring node packages for each API //
+var Spotify = require("node-spotify-api");
+var axios = require("axios");
+var moment = require("moment");
+moment().format();
 
 
-// variable for Spotify API key and ids //
+// Variable for Spotify API key and ids //
 var spotify = new Spotify(keys.spotify);
-console.log(spotify);
 
-// Variables for both the command and the search query //
-var command = process.argv[2];
-var search = process.argv[3];
+// Variables for both the command and the command string query //
+var commandType = process.argv[2];
+console.log(commandType)
+var commandStr = process.argv[3];
+console.log(commandStr);
 
-// Variables for search query URLS for the different APIS //
-var querySpotify = "https://api.spotify.com/v1/search?q" + search + "&type=track" + spotify; 
-var queryOMDB = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy";
-var queryBIT = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp";
 
-// Function for Spotify API search and data return //
-spotify.spotify(search)
-.then(function(spotifyRes){
-    console.log(spotifyRes)
-})
-.catch(function(err){
-    console.log(err);
-});
+// Input arguments logic //
+switch(commandType) {
+    case 'spotify-this-song':
+        callSpotify(commandStr);
+        break;
 
-// Function for OMDB API search and data return //
-omdb.omdb(search)
-.then(function(omdbRes){
-    console.log(omdbRes)
-})
-.catch(function(err){
-    console.log(err)
-});
+    case 'movie-this':
+        callOMDB(commandStr);
+        break;
 
-// Function for Bands in Town API search and data return //
-bandsInTown.bandsInTown(search)
-.then(function(bitRes){
-    console.log(bitRes)
-})
-.catch(function(err){
-    console.log(err)
-});
+    case 'concert-this':
+        callBandsInTown(commandStr);
+        break;
+    
+    case 'do-what-it-says':
+        callWhatItSays();
+        break;
+};
+
+// Function for Spotify //
+function callSpotify(commandStr) {
+    if(!commandStr){
+        commandStr = "hello";
+    }
+    spotify
+    .search({ type: 'track', query: commandStr })
+    .then(function(response){
+        for (var i = 0; i < 5; i++) {
+            var songs = 
+                "-------------------------------------------------------------" +
+                "\nArtist(s): " + response.tracks.items[i].artists[0].name +
+                "\nSong Name: " + response.tracks.items[i].name +
+                "\nAlbum Name: " + response.tracks.items[i].album.name;
+            console.log(songs);
+        }
+    })
+    .catch(function(err){
+        console.log(err);
+    });
+}
+
+// Function for OMDB //
+function callOMDB(commandStr) {
+    if (!commandStr){
+        commandStr = "lost in space";
+    }
+    axios.get("https://omdpapi.com/?t=" + commandStr + "&y=&plot=short&apikey=trilogy")
+    .then(function(response){
+        var movies = 
+            "-------------------------------------------------------------" +
+            "\nMovie Title: " + response.data.Title +
+            "\nYear of Release: " + response.data.Year +
+            "\nIMDB Rating: " + response.data.imdbRating +
+            "\nCountry Produced: " + response.data.Country +
+            "\nLanguage: " + response.data.Language +
+            "\nPlot: " + response.data.Plot;
+        console.log(movies);
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+}
+
+// Function for Bands in Town //
+function callBandsInTown(commandStr) {
+    axios.get("https://rest.bandsintown.com/artists/" + commandStr + "/events?app_id=codingbootcamp")
+    .then(function(response){
+        for (var i = 0; i < response.data.length; i++) {
+            var datetime = response.data[i].datetime;
+            var dateArray = datetime.split('T');
+
+            var concerts = 
+                "-------------------------------------------------------------" +
+                "\nVenue Name: " + response.data[i].venue.name +
+                "\nVenue Location: " + response.data[i].venue.city +
+                "\nDate of the Event: " + moment(dateArray[0], "MM-DD-YYYY"); 
+        console.log(concerts);
+        }
+    })
+    .catch(function(error){
+        console.log(error);
+    });
+}
+
+// Function for do this //
+function callWhatItSays(commandStr) {
+    fs.readFile("random.txt", "utf8", function (error, data){
+        if (error) {
+            return console.log(error);
+        }
+        var dataArray = data.split(',');
+        spotifyThis(dataArray[0], dataArray[1]);
+    })
+}
